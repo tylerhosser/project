259206d0ab22f1dbe857fdd1190fecfd5c22d60f
Tyler Hosser <tyler.hosser@libertymutual.com>
2024-04-22 09:29:59 -0400

    completed stage01 basic file server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');  // for handling file paths
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4000;  // use env var or default to 4000
+const port = process.env.PORT || 4000;
 
-// Set the static directory to serve files from
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
