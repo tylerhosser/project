520a25becba06a69c2c1491ade17ceefc43bb4ff
Tyler Hosser <tyler.hosser@libertymutual.com>
2024-04-22 10:27:56 -0400

    completed stage02 add getCustomer

---
+++ data-access.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'custdb';
+const baseUrl = "mongodb://127.0.0.1:27017";
+const collectionName = "customers"
+const connectString = baseUrl + "/" + dbName; 
+let collection;
+
+async function dbStartup() {
+    const client = new MongoClient(connectString);
+    await client.connect();
+    collection = client.db(dbName).collection(collectionName);
+}
+
+async function getCustomers() {
+    return await collection.find().toArray();
+}
+
+dbStartup();
+module.exports = { getCustomers };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const da = require("./data-access");
 const path = require('path');
 
 const app = express();
@@ -9,3 +10,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+app.get("/customers", async (req, res) => {
+    const cust = await da.getCustomers();
+    res.send(cust);
+   });
